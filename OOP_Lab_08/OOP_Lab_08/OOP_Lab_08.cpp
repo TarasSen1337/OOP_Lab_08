22ffcb38d2d4e02a18089edb5e20d541c737ab7e
Tarassen1337 <senyk.taras.chapm@gmail.com>
2020-05-27 16:00:08 +0300

    singleton

--- OOP_Lab_08/OOP_Lab_08/OOP_Lab_08.cpp
+++ OOP_Lab_08/OOP_Lab_08/OOP_Lab_08.cpp
@@ -1,20 +1,58 @@
-// OOP_Lab_08.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <string>
 
-int main()
+using namespace std;
+
+
+class FruitSingleton
 {
-    std::cout << "Hello World!\n";
+private:
+
+	static FruitSingleton* singleObject;
+	static int count;
+
+
+	int amount;
+	string name;
+
+	FruitSingleton(int a, string n)
+	{
+		name = n;
+		amount = a;
+		count++;
+	}
+
+public:
+	static FruitSingleton& getInstance(int a, string n)
+	{
+		if (count == 0)
+		{
+			singleObject = new FruitSingleton(a, n);
+		}
+		return *singleObject;
+	};
+
+	int getAmount()
+	{
+		return amount;
+	}
+	string getName() {
+		return name;
+	}
+};
+
+int FruitSingleton::count = 0;
+FruitSingleton* FruitSingleton::singleObject = 0;
+
+int main() {
+
+	FruitSingleton& testobject = FruitSingleton::getInstance(5, "apples");
+
+	cout << "There are " << testobject.getAmount() << " " << testobject.getName() << endl;
+
+	testobject = FruitSingleton::getInstance(3, "oranges");
+
+	cout << "There are " << testobject.getAmount() << " " << testobject.getName() << endl;
+
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
